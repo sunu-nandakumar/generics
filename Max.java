fc317ea3db05f5e6ce890237202cad92334e1614
sunu-nandakumar <appu1998.nandakumar@gmail.com>
2021-06-21 16:27:25 +0530

    adding the last uc

--- Max.java
+++ Max.java
@@ -1,44 +1,52 @@
 package com.bridgelabz.generics;
 
-public class Max {
-  // using generic for making the code more streamline
-	
-	public static <T extends Comparable<T>> T checkMax(T x, T y, T z) {
-		T max = x;
-		if(y.compareTo(max) > 0) {
-			max = y; // y is maximum now
-		}
-		if(z.compareTo(max) > 0) {
-			max = z; // z is maximum now
-		}
-		return max; // returns maximum value
-	}
-	
-	public static <T extends Comparable<T>> T checkMax_With_MoreThan_ThreeParameters(T... elements) {
-		T max = elements[0];
-		for(T maxParametereter : elements) {
-			if(maxParametereter.compareTo(max) > 0) {
-				max = maxParametereter;
-			}
-		}
-		return max;
-	}
-			public static void main(String[] args) {
-				Integer value= checkMax(48,95,35);
-				Double result = checkMax(15.4,12.5,55.5);
-				String cars = checkMax("sonet", "seltos", "nexon");
-				Integer valueForInteger = checkMax_With_MoreThan_ThreeParameters(55,43,85,40,93);
-				Double resultForDouble = checkMax_With_MoreThan_ThreeParameters(19.8, 25.8, 23.0,24.0,50.2);
-				String fruits = checkMax_With_MoreThan_ThreeParameters("Apple", "Peach", "Banana", "Mango", "Orange"); 
-				
-				System.out.println(value);
-				System.out.println(result);
-				System.out.println(cars);
-				System.out.println(valueForInteger);
-				System.out.println(resultForDouble);
-				System.out.println(fruits);
-			}
+public class Max<T extends Comparable<T>> {
 
+	T a, b, c;
 
+	public Max(T a, T b, T c) {
+		this.a = a;
+		this.b = b;
+		this.c = c;
+	}
+
+	// checking which object is the largest
+	public static <T extends Comparable<T>> T checkMax(T a, T b, T c) {
+		T max = a;
+		if (b.compareTo(max) > 0) {
+			max = b; // y is largest thus for
+		}
+		if (c.compareTo(max) > 0) {
+			max = c; // z is largest now
+		}
+		printMax(max);
+		return max; // returns maximum
+	}
+
+	public static String testMax(String x, String y, String z) {
+		String max = x;
+		if (y.compareTo(max) > 0) {
+			max = y; // y is the largest
+		}
+		if (z.compareTo(max) > 0) {
+			max = z; // z is the largest
+		}
+		printMax(max);
+		return max; // returns the largest so for
+	}
+
+	public static <T> void printMax(T max) {
+		System.out.println("Max of Three  is" + max);
+	}
+
+	public static void main(String[] args) {
+		Integer value = checkMax(48, 95, 35);
+		Double result = checkMax(15.4, 12.5, 55.5);
+		String first = "seltos", second = "sonet", third = "nexon";
+		Max.testMax(first, second, third);
+		printMax(value);
+		printMax(result);
+
+	}
 
 }
